009bad4d243e7005b44ab53dc31f9b19defc65d4
Ben Tomlin <bentomlin101@gmail.com>
2020-05-03 21:32:34 -0700

    added jump and new background

--- Player.gd
+++ Player.gd
@@ -1,16 +1,17 @@
 extends KinematicBody2D
 
-export var speed = 200 # how fast he can run
-export var acc = 20 # how fast he can change directions
+export var speed = 200 # how fast you can run
+export var acc = 20 # how fast you can change directions
+export var walk_slowness = 0.3
 export var gravity = 50
-var Coin = preload("res://Coin.tscn")
+export var jump_height = 20
 
 onready var velocity = Vector2.ZERO
 onready var anim_player = $AnimatedSprite/AnimationPlayer
+onready var squish_stretch_player = $AnimatedSprite/SquishStretchPlayer
 onready var particle_emitter = $Particles
-
-
-
+onready var raycast = $RayCast
+onready var was_on_floor = is_on_floor()
 
 func _physics_process(delta):
 	var dir = 0
@@ -19,14 +20,25 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		dir -= 1
 	
-	particle_emitter.emitting = dir != 0 && is_on_floor()
-	anim_player.play("stand" if dir == 0 else "run")
+	particle_emitter.emitting = dir != 0 and is_on_floor()
+	
+	var anim = ""
+	if not is_on_floor():
+		anim = "run"
+	elif dir == 0:
+		anim = "stand"
+	elif Input.is_action_pressed("sneak"):
+		anim = "walk"
+	else:
+		anim = "run"
+	
+	anim_player.play(anim)
 	
 	if dir == 1:
-		velocity.x = min(velocity.x + acc, speed)
+		velocity.x = min(velocity.x + acc, get_move_vel())
 		$AnimatedSprite/Sprite.flip_h = false
 	elif dir == -1:
-		velocity.x = max(velocity.x - acc, -speed)
+		velocity.x = max(velocity.x - acc, -get_move_vel())
 		$AnimatedSprite/Sprite.flip_h = true
 	elif velocity.x > 0:
 		velocity.x -= acc
@@ -34,13 +46,22 @@ func _physics_process(delta):
 		velocity.x += acc
 	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			velocity.y = -20 * gravity
-		elif velocity.y > 0:
-			particle_emitter.amount = 50;
-			yield(get_tree().create_timer(0.2), "timeout");
-			particle_emitter.amount = 10;
+		if Input.is_action_pressed("jump"):
+			velocity.y = -jump_height * gravity
+			squish_stretch_player.play("stretch")
+		elif not was_on_floor:
+			squish_stretch_player.play("squish")
+	
+	was_on_floor = is_on_floor()
 	
 	velocity.y += gravity
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+
+func get_move_vel():
+	return speed * (walk_slowness if Input.is_action_pressed("sneak") else 1)
+	
+	
+	
