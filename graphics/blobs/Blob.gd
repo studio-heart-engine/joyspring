d680bc04c95eedc1424ca33a364beec54888633d
Ben Tomlin <bentomlin101@gmail.com>
2020-05-17 14:18:06 -0700

    added blobs

---
+++ graphics/blobs/Blob.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+export (Texture) var texture
+
+func _ready():
+	$NoiseOffset/Outline.texture = texture
+	$NoiseOffset/Sprite.texture = texture
---
+++ graphics/blobs/blob_noise_offset.gd
@@ -0,0 +1,19 @@
+tool
+
+extends Position2D
+
+var noise
+var t1 = 0
+var t2 = 0
+
+func _ready():
+	noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.octaves = 1
+	noise.period = 20.0
+
+func _process(delta):
+	if not Engine.editor_hint:
+		position = 0.3 * Vector2(noise.get_noise_2d(t1, 0) - 0.5, noise.get_noise_2d(0, t2) - 0.5)
+		t1 += 1.3
+		t2 += 1.3
