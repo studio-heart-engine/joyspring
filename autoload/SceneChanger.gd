8bee9dd25a472f6caa4e23bbb5ac60c162ee76ed
Ben Tomlin <bentomlin101@gmail.com>
2020-05-22 16:27:59 -0700

    finished tutorial level first prototype

--- autoload/SceneChanger.gd
+++ autoload/SceneChanger.gd
@@ -7,12 +7,22 @@ func _ready():
 	color_rect.set_visible(false)
 
 
-func change_scene(path, color = Color.black):
+func change_scene(path: String, color = Color.black):
 	color_rect.color = color
 	color_rect.set_visible(true)
 	$AnimationPlayer.play("fade")
 	yield($AnimationPlayer, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
+	$AnimationPlayer.play_backwards("fade")
+	yield($AnimationPlayer, "animation_finished")
+	color_rect.set_visible(false)
+
+func change_scene_to(scene: PackedScene, color = Color.black):
+	color_rect.color = color
+	color_rect.set_visible(true)
+	$AnimationPlayer.play("fade")
+	yield($AnimationPlayer, "animation_finished")
+	assert(get_tree().change_scene_to(scene) == OK)
 	$AnimationPlayer.play_backwards("fade")
 	yield($AnimationPlayer, "animation_finished")
 	color_rect.set_visible(false)
--- platformer/PlatformerTemplate.gd
+++ platformer/PlatformerTemplate.gd
@@ -1,5 +1,7 @@
+tool
 extends Node2D
 
-func _ready():
-	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass
+export (PackedScene) var next_level
+
+func _get_configuration_warning():
+	return "Next Level property can't be empty!" if not next_level else ""
