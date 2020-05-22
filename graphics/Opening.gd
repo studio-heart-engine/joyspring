c87e0c17cbeca61ec9dbb6060a7ab787553e64c3
Ben Tomlin <bentomlin101@gmail.com>
2020-05-21 18:13:43 -0700

    started new opening animation

--- graphics/Opening.gd
+++ graphics/Opening.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
-		SceneChanger.change_scene("res://gui/Menu.tscn", Color.white)
+		SceneChanger.change_scene("res://gui/Menu.tscn")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	SceneChanger.change_scene("res://gui/Menu.tscn", Color.white)
+	SceneChanger.change_scene("res://gui/Menu.tscn")
