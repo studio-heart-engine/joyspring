384a0e8374aaa5a03899195ed5eb7080e0da7c72
Ben Tomlin <bentomlin101@gmail.com>
2020-05-22 15:39:56 -0700

    removed old menu anim, started tutorial level

--- gui/Menu.gd
+++ gui/Menu.gd
@@ -1,13 +1,6 @@
 extends Control
 
 
-func _ready():
-	if not globals.has_played_startup_animation:
-		$AnimationPlayer.play("startup")
-		$AnimationPlayer.advance(5)
-		globals.has_played_startup_animation = true
-
-
 func _input(event):
 	if event.is_action_pressed("ui_select"):
 		begin()
@@ -17,6 +10,8 @@ func _on_BeginButton_pressed():
 	begin()
 
 func begin():
+	$AnimationPlayer.play_backwards("startup")
+	$AnimationPlayer.stop(false)
 	$AnimationPlayer.play("start-fall")
 	yield($AnimationPlayer, "animation_finished")
 	SceneChanger.change_scene("res://platformer/levels/Level01.tscn")
