c4f63685e3e50aa09bc8812841521c9a9bd9cc17
Ben Tomlin <bentomlin101@gmail.com>
2020-06-29 02:04:55 -0700

    bug fixes, slo mo effects, level 7

--- platformer/SlowTime.gd
+++ platformer/SlowTime.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 export (String) var action = ""
 export (float, 0, 2) var wait_time = 0.4
@@ -12,12 +12,16 @@ func _ready():
 	set_process(false)
 	yield(get_tree().create_timer(wait_time), "timeout")
 	set_process(true)
+	$AnimationPlayer.play("slow")
 
 func _process(delta):
 	if not done:
 		Engine.time_scale *= 0.9
 
 func resume():
+	if done:
+		return
 	done = true
 	Engine.time_scale = 1
+	$AnimationPlayer.play_backwards("slow")
 	emit_signal("resumed")
