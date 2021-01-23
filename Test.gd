397ad2ed339c791e0ca058589f9d81406a291931
Ben Tomlin <bentomlin101@gmail.com>
2021-01-23 11:13:51 -0800

    imports

---
+++ Test.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var img = load("res://graphics/tilesets/dawn/dirt.png")
+
+func _ready():
+	print(load("res://graphics/tilesets/dawn/dirt.png"))
--- gui/HUD.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func _ready():
-	Events.connect("joy_collected", self, "update_score_label")
-
-func update_score_label():
-	$MarginContainer/ScoreLabel.text = "Joy: " + str(PlayerData.score)
-	$AnimationPlayer.play("joy-label-blink")
