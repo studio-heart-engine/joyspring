6463ba7246ca67703a5102781028eaa88edecdc0
Ben Tomlin <bentomlin101@gmail.com>
2021-01-30 13:43:53 -0800

    shenanagins

--- graphics/HDSceneChanger.gd
+++ graphics/HDSceneChanger.gd
@@ -1,6 +1,8 @@
 extends Node
 
-export (PackedScene) var opening = preload('res://platformer/levels/Level_01.tscn')
+export (PackedScene) var opening
+
+onready var sub_viewport = $'/root/HDWrapper/ViewportContainer/Viewport'
 
 var packed_current_scene
 var current_scene
@@ -9,13 +11,20 @@ var current_scene
 func _ready():
 	current_scene = opening.instance()
 	packed_current_scene = opening
-	get_node('/root/HDWrapper/ViewportContainer/Viewport').add_child(current_scene)
+	sub_viewport.add_child(current_scene)
+
+
+func change_scene(scene: String):
+	current_scene.queue_free()
+	current_scene = load(scene).instance()
+	packed_current_scene = scene
+	sub_viewport.add_child(current_scene)
 
 func change_scene_to(scene: PackedScene):
 	current_scene.queue_free()
 	current_scene = scene.instance()
 	packed_current_scene = scene
-	get_node('/root/HDWrapper/ViewportContainer/Viewport').add_child(current_scene)
+	sub_viewport.add_child(current_scene)
 	
 func reload_scene():
 	change_scene_to(packed_current_scene)
