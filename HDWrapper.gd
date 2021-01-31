a51ad95d89a0efa35aee10cb843c9f8f48736e05
Ben Tomlin <bentomlin101@gmail.com>
2021-01-30 21:28:58 -0800

    no message

--- HDWrapper.gd
+++ HDWrapper.gd
@@ -18,5 +18,5 @@ func _process(delta):
 		for child in children:
 			if child.name.substr(0, 4) == "Text":
 				var coord = child.get_global_transform_with_canvas().origin
-				var path = "Text/Control/Label" + child.name.right(4)
+				var path = "Text/Control/Label1" + child.name.right(4)
 				get_node(path).rect_position = coord * scaling
