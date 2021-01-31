540f0a7dd7e8df87bd24b5e2620ecbc391747bf5
BitLorax <willjhliang@gmail.com>
2021-01-30 23:22:36 -0800

    Fixed merging overwrite

--- autoload/SceneChanger.gd
+++ autoload/SceneChanger.gd
@@ -8,16 +8,16 @@ signal scene_changed
 func _ready():
 	color_rect.set_visible(false)
 
-func change_scene(path: String, color = Color.black):
-	color_rect.color = color
-	color_rect.set_visible(true)
-	$AnimationPlayer.play("fade")
-	yield($AnimationPlayer, "animation_finished")
-	HDSceneChanger.change_scene(path)
-	call_deferred("emit_signal", "scene_changed")
-	$AnimationPlayer.play_backwards("fade")
-	yield($AnimationPlayer, "animation_finished")
-	color_rect.set_visible(false)
+#func change_scene(path: String, color = Color.black):
+#	color_rect.color = color
+#	color_rect.set_visible(true)
+#	$AnimationPlayer.play("fade")
+#	yield($AnimationPlayer, "animation_finished")
+#	HDSceneChanger.change_scene(path)
+#	call_deferred("emit_signal", "scene_changed")
+#	$AnimationPlayer.play_backwards("fade")
+#	yield($AnimationPlayer, "animation_finished")
+#	color_rect.set_visible(false)
 
 func change_scene_to(scene: PackedScene, color = Color.black):
 	color_rect.color = color
