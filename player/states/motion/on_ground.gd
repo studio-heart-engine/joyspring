73ccf7247e54d6cbbe26bbff986f18c790c80a99
Ben Tomlin <bentomlin101@gmail.com>
2021-02-08 23:30:46 -0800

    fnished fixing dash

--- player/states/motion/on_ground.gd
+++ player/states/motion/on_ground.gd
@@ -34,6 +34,6 @@ func handle_input(event):
 	.handle_input(event)
 	if event.is_action_pressed("up"):
 		emit_signal("finished", "jump")
-	if event.is_action_pressed('dash') and owner.can_dash and self.input_direction != Vector2.ZERO:
-		emit_signal('finished', 'fall')  # So dash goes to fall after it's complete
+	if event.is_action_pressed('dash') and owner.can_dash and self.input_direction != Vector2.ZERO and $"../../..".dash_enabled:
+		emit_signal('finished', 'fall')  # So dash goes to fall after it's complete in case they dash from ground to air
 		emit_signal('finished', 'dash')
