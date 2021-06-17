2a987a7a4b1497e1b4947d5389c42ab371d35bb7
BitLorax <willjhliang@gmail.com>
2021-06-16 18:41:26 -0700

    Fixed controls screen

--- graphics/HDSceneChanger.gd
+++ graphics/HDSceneChanger.gd
@@ -1,7 +1,7 @@
 extends Node
 
-#export (PackedScene) var opening = preload('res://graphics/Opening.tscn')
-export (PackedScene) var opening = preload('res://platformer/levels/Level_17.tscn')
+export (PackedScene) var opening = preload('res://graphics/Opening.tscn')
+#export (PackedScene) var opening = preload('res://platformer/levels/Level_16.tscn')
 #export (PackedScene) var opening = preload('res://platformer/PlatformerLayeredTemplateTEST.tscn')
 #export (PackedScene) var opening = preload('res://gui/LevelSelect.tscn')
 
