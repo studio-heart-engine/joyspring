b100b1683c11a148e9f8b7a0f9a9e9ecebd2b2ef
BitLorax <willjhliang@gmail.com>
2021-07-31 17:36:55 -0700

    Fixed light occlusion bug, reviewed Ben's first two levels

--- graphics/HDSceneChanger.gd
+++ graphics/HDSceneChanger.gd
@@ -1,7 +1,7 @@
 extends Node
 
 #export (PackedScene) var opening = preload('res://graphics/Legend.tscn')
-export (PackedScene) var opening = preload('res://platformer/levels/Finished_Level_01.tscn')
+export (PackedScene) var opening = preload('res://platformer/levels/Finished_Level_05.tscn')
 #export (PackedScene) var opening = preload('res://platformer/PlatformerLayeredTemplate.tscn')
 #export (PackedScene) var opening = preload('res://gui/LevelSelect.tscn')
 
--- player/glow.gd
+++ player/glow.gd
@@ -4,7 +4,6 @@ func _ready():
 	Events.connect("joy_attached_to_cape", self, "glow")
 	Events.connect("player_dashed", self, "_on_player_dashed")
 
-
 func glow():
 	if $GlowAnimationPlayer.current_animation != "dash-glow":
 		$GlowAnimationPlayer.play("glow")
